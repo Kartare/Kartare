9b10ef5fa0f1df552eb9426bc2220bee4e12e73b
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2024-04-20 14:08:37 +0000

    Finalize current version of home page

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default async function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <header>
         <h1 className="text-7xl text-center">Kartare</h1>
         <h2 className="text-2xl mt-10 text-center text-slate-800">AI augmented roadtrip planner</h2>
-        <div className="mt-6 flex justify-center">
+        {/* <div className="mt-6 flex justify-center">
           <Link href="/planner">
             <Button variant={"outline"}>
               <ArrowRight width={16} height={16} className="mr-2" />
@@ -19,15 +19,15 @@ export default async function Home() {
         </div>
         <div className="mt-2 flex justify-center">
           <p className="italic text-sm">Planner is currently in <u>static</u> mode.</p>
-        </div>
+        </div> */}
       </header>
 
       <div className="w-full flex justify-center fixed bottom-4 px-4">
-        <div className="w-full max-w-lg flex flex-row">
-          <div className="w-1/2">
+        <div className="w-full max-w-lg flex flex-col-reverse sm:flex-row">
+          <div className="sm:w-1/2 w-full text-center sm:mt-0 mt-4">
             Copyright &copy; 2024 &ndash; Kartare
           </div>
-          <div className="w-1/2">
+          <div className="sm:w-1/2 w-full flex justify-center">
             <Link href="https://github.com/kartare/kartare" className="flex items-center" target="_blank">
               <Github width={16} height={16} className="mr-2" />
               Contribute via GitHub
