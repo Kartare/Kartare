b6252d11a83986919beac9f019b00b74cf0bdd4b
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2024-04-20 08:53:40 +0000

    Try work llm

--- lib/llm.ts
+++ lib/llm.ts
@@ -2,11 +2,16 @@ import { ChatSession, CompletionService } from "langxlang";
 
 const llm = new CompletionService({ gemini: process.env.GEMINI!, openai: "" });
 
-export async function Talk() {
-const [response] = await llm.requestCompletion(
+export async function Talk(): Promise<string> {
+
+  const response = await llm.requestCompletion(
     'gemini-1.0-pro',         //  Model name
     '',                       //  System prompt (optional)
     'Tell me about yourself'  //  User prompt
   )
-  console.log(response.text);
+  const result = response.map(o => o.text).join(', ');
+
+  console.log(result);
+
+  return result;
 }
