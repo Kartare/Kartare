470d398017a243af5c682611b87ebd038a5753ca
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2024-04-26 20:32:45 +0000

    Add basic authentication with Kinde

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,24 +1,44 @@
+
+
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Github } from "lucide-react";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight, Github, LogIn } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
+
+  const { isAuthenticated } = await getKindeServerSession();
+  const isLoggedIn = await isAuthenticated();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <header>
         <h1 className="text-7xl text-center">Kartare</h1>
         <h2 className="text-2xl mt-10 text-center text-slate-800">AI augmented roadtrip planner</h2>
+
         <div className="mt-6 flex justify-center">
-          <Link href="/planner">
-            <Button variant={"outline"}>
-              <ArrowRight width={16} height={16} className="mr-2" />
-              Go to planner
-            </Button>
-          </Link>
-        </div>
-        <div className="mt-2 flex justify-center">
           <p className="italic text-sm">Planner is currently in <u>static</u> mode.</p>
+        </div>
+
+        <div className="mt-2 flex justify-center">
+
+          {isLoggedIn && (
+            <Link href="/planner">
+              <Button variant={"outline"}>
+                <ArrowRight width={16} height={16} className="mr-2" />
+                Login
+              </Button>
+            </Link>
+          )}
+          {!isLoggedIn && (
+            <LoginLink postLoginRedirectURL="/planner">
+              <Button variant={"outline"}>
+                <LogIn width={16} height={16} className="mr-2" />
+                Login
+              </Button>
+            </LoginLink>
+          )}
         </div>
       </header>
 
