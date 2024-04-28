ea4e747dfc6a5204539c9b113d0f438e198a229f
Dick Wolff <dckwlff@gmail.com>
2024-04-28 12:12:18 +0200

    Fix activity align

--- app/(main)/(planner)/_components/activty-listitem.tsx
+++ app/(main)/(planner)/_components/activty-listitem.tsx
@@ -1,15 +1,19 @@
 import { Activity } from "@/app/models/activity";
-import { Dot } from "lucide-react";
 
 const ActivityListItem = ({ activity }: { activity: Activity }) => {
 
     return (
-        <div className="flex flex-row items-center">
-            <Dot />
-            <span className="text-xl">{activity.name}</span>
-            {activity.price && (
-                <span className="text-sm ml-4">&euro; {activity.price}</span>
-            )}
+        <div className="flex flex-row items-start">
+            <ul className="list-disc pl-5">
+                <li>
+                    <div className="flex flex-row items-center">
+                        <span className="text-xl">{activity.name}</span>
+                        {activity.price && (
+                            <span className="text-sm ml-4">&euro; {activity.price}</span>
+                        )}
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
